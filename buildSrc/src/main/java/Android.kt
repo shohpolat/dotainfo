228dc8b94f1e77846131b42c7b65cb3a7d41fd19
shohpolat <shohruxbeka@mail.ru>
2023-05-21 11:39:01 +0500

    initial commit

---
+++ buildSrc/src/main/java/Android.kt
@@ -0,0 +1,10 @@
+object Android {
+    const val nameSpace = "com.shohpolat.dotainfoapp"
+    const val appId = "com.shohpolat.dotainfoapp"
+    const val compileSdk = 33
+    const val buildTools = "30.0.3"
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
