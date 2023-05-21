06cbd1bbe8fd909b86d690a4ddddeb3afbffd465
shohpolat <shohruxbeka@mail.ru>
2023-05-21 12:35:31 +0500

    create generic gradle files

--- hero/ui-heroList/build.gradle.kts
+++ hero/ui-heroList/build.gradle.kts
@@ -1,53 +1,6 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+apply{
+    from("$rootDir/android-library-build.gradle")
 }
+dependencies{
 
-android {
-    namespace = "com.shohpolat.ui_herolist"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-
-    implementation(AndroidX.coreKtx)
-    implementation(AndroidX.appCompat)
-    implementation(AndroidX.lifecycleVmKtx)
-
-    implementation(Compose.activity)
-    implementation(Compose.ui)
-    implementation(Compose.material)
-    implementation(Compose.tooling)
-    implementation(Compose.navigation)
-    implementation(Compose.hiltNavigation)
-
-    implementation(Google.material)
-    implementation(Junit.junit4)
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
 }
